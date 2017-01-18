f64a37c70821c7023e3dc52a7c5de436195e52b1
CodeDead <admin@codedead.com>
2017-01-18 07:56:48 +0100

    * Added library for displaying GIF animations

--- AniView/Views/MainWindow.xaml.cs
+++ AniView/Views/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using XamlAnimatedGif;
+using DownloadProgressEventArgs = XamlAnimatedGif.DownloadProgressEventArgs;
+using Path = System.IO.Path;
 
 namespace AniView.Views
 {
@@ -20,6 +16,20 @@ namespace AniView.Views
     /// </summary>
     public partial class MainWindow
     {
+        private List<string> _images = new List<string>();
+        private int _current;
+        private Animator _animator;
+        private bool _useDefaultRepeatBehavior = true;
+        private bool _repeatForever;
+        private bool _useSpecificRepeatCount;
+        private int _repeatCount = 3;
+        private bool _completed;
+        private RepeatBehavior _repeatBehavior;
+        private bool _autoStart = true;
+        private bool _isDownloading;
+        private int _downloadProgress;
+        private bool _isDownloadProgressIndeterminate;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,11 +43,237 @@ namespace AniView.Views
         {
             string[] args = Environment.GetCommandLineArgs();
             if (args.Length <= 1) return;
+            if (!File.Exists(args[1])) return;
 
-            if (File.Exists(args[1]))
+            LoadImage(args[1]);
+        }
+
+        /// <summary>
+        /// Load the path of all GIF images into a list and determine the current position
+        /// </summary>
+        private void LoadImage(string path)
+        {
+            if (path == null) return;
+
+            AnimationBehavior.SetSourceUri(ImgView, new Uri(path));
+            _images = new List<string>();
+            ImgPause.Source = new BitmapImage(new Uri("/AniView;component/Resources/Images/pin.png", UriKind.Relative));
+            SizeToContent = SizeToContent.WidthAndHeight;
+
+            foreach (string s in Directory.GetFiles(Path.GetDirectoryName(path), "*.gif", SearchOption.TopDirectoryOnly))
             {
-                ImgView.Source = new BitmapImage(new Uri(args[1]));
+                if (!_images.Contains(s))
+                {
+                    _images.Add(s);
+                }
             }
+        }
+
+        private void BtnRight_Click(object sender, RoutedEventArgs e)
+        {
+            _current++;
+            if (_current > _images.Count - 1)
+            {
+                _current = 0;
+            }
+            LoadImage(_images[_current]);
+        }
+
+        private void BtnLeft_Click(object sender, RoutedEventArgs e)
+        {
+            _current--;
+            if (_current < 0)
+            {
+                _current = _images.Count - 1;
+            }
+            LoadImage(_images[_current]);
+        }
+
+        private void BtnPause_Click(object sender, RoutedEventArgs e)
+        {
+            if (_animator == null) return;
+            if (_animator.IsPaused)
+            {
+                _animator.Play();
+                ImgPause.Source = new BitmapImage(new Uri("/AniView;component/Resources/Images/pin.png", UriKind.Relative));
+            }
+            else
+            {
+                _animator.Pause();
+                ImgPause.Source = new BitmapImage(new Uri("/AniView;component/Resources/Images/replay.png", UriKind.Relative));
+            }
+        }
+
+        private void CurrentFrameChanged(object sender, EventArgs e)
+        {
+            if (_animator != null)
+            {
+                //sldPosition.Value = _animator.CurrentFrameIndex;
+            }
+        }
+
+        private void AnimationCompleted(object sender, EventArgs e)
+        {
+            Completed = true;
+        }
+
+        public bool UseDefaultRepeatBehavior
+        {
+            get { return _useDefaultRepeatBehavior; }
+            set
+            {
+                _useDefaultRepeatBehavior = value;
+                OnPropertyChanged("UseDefaultRepeatBehavior");
+                if (value)
+                    RepeatBehavior = default(RepeatBehavior);
+            }
+        }
+
+        public bool RepeatForever
+        {
+            get { return _repeatForever; }
+            set
+            {
+                _repeatForever = value;
+                OnPropertyChanged("RepeatForever");
+                if (value)
+                    RepeatBehavior = RepeatBehavior.Forever;
+            }
+        }
+
+        private void AnimationBehavior_OnDownloadProgress(DependencyObject d, DownloadProgressEventArgs e)
+        {
+            IsDownloading = true;
+            if (e.Progress >= 0)
+            {
+                DownloadProgress = e.Progress;
+                IsDownloadProgressIndeterminate = false;
+            }
+            else
+            {
+                IsDownloadProgressIndeterminate = true;
+            }
+        }
+
+        public bool UseSpecificRepeatCount
+        {
+            get { return _useSpecificRepeatCount; }
+            set
+            {
+                _useSpecificRepeatCount = value;
+                OnPropertyChanged("UseSpecificRepeatCount");
+                if (value)
+                    RepeatBehavior = new RepeatBehavior(RepeatCount);
+            }
+        }
+
+        public int RepeatCount
+        {
+            get { return _repeatCount; }
+            set
+            {
+                _repeatCount = value;
+                OnPropertyChanged("RepeatCount");
+                if (UseSpecificRepeatCount)
+                    RepeatBehavior = new RepeatBehavior(value);
+            }
+        }
+
+        public bool Completed
+        {
+            get { return _completed; }
+            set
+            {
+                _completed = value;
+                OnPropertyChanged("Completed");
+            }
+        }
+
+        public RepeatBehavior RepeatBehavior
+        {
+            get { return _repeatBehavior; }
+            set
+            {
+                _repeatBehavior = value;
+                OnPropertyChanged("RepeatBehavior");
+                Completed = false;
+            }
+        }
+
+        public bool AutoStart
+        {
+            get { return _autoStart; }
+            set
+            {
+                _autoStart = value;
+                OnPropertyChanged("AutoStart");
+            }
+        }
+
+        public bool IsDownloading
+        {
+            get { return _isDownloading; }
+            set
+            {
+                _isDownloading = value;
+                OnPropertyChanged("IsDownloading");
+            }
+        }
+
+        public int DownloadProgress
+        {
+            get { return _downloadProgress; }
+            set
+            {
+                _downloadProgress = value;
+                OnPropertyChanged("DownloadProgress");
+            }
+        }
+
+        public bool IsDownloadProgressIndeterminate
+        {
+            get { return _isDownloadProgressIndeterminate; }
+            set
+            {
+                _isDownloadProgressIndeterminate = value;
+                OnPropertyChanged("IsDownloadProgressIndeterminate");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void AnimationBehavior_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            if (_animator != null)
+            {
+                _animator.CurrentFrameChanged -= CurrentFrameChanged;
+                _animator.AnimationCompleted -= AnimationCompleted;
+            }
+
+            _animator = AnimationBehavior.GetAnimator(ImgView);
+
+            if (_animator != null)
+            {
+                _animator.CurrentFrameChanged += CurrentFrameChanged;
+                _animator.AnimationCompleted += AnimationCompleted;
+                //sldPosition.Value = 0;
+                //sldPosition.Maximum = _animator.FrameCount - 1;
+                //SetPlayPauseEnabled(_animator.IsPaused || _animator.IsComplete);
+            }
+        }
+
+        private void AnimationBehavior_OnError(DependencyObject d, AnimationErrorEventArgs e)
+        {
+            if (e.Kind == AnimationErrorKind.Loading)
+                IsDownloading = false;
+
+            MessageBox.Show($"An error occurred ({e.Kind}): {e.Exception}");
         }
     }
 }
