b928a2577cb82ee253499c1ffa3af58158ae4f6a
CodeDead <admin@codedead.com>
2018-01-07 23:31:48 +0100

    * Upgraded .NET to 4.7.1

--- AniView/Properties/AssemblyInfo.cs
+++ AniView/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodeDead")]
 [assembly: AssemblyProduct("AniView")]
-[assembly: AssemblyCopyright("Copyright © CodeDead 2017")]
+[assembly: AssemblyCopyright("Copyright © CodeDead 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
